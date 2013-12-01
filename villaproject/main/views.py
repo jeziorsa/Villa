0a2479a62ab2cc8e6260fea4471c465e2f468fd5
Adam Jeziorski <biuro@atsa.pl>
2013-12-01 17:27:16 +0100

    really basic auth

--- villaproject/main/views.py
+++ villaproject/main/views.py
@@ -1,4 +1,52 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.core.context_processors import csrf
 
 def home(request):
+
+
+    if request.method == 'POST':
+        print 'przed'
+        c = {}
+        c.update(csrf(request))
+        username = request.POST['username']
+        password = request.POST['password']
+        print username
+        print password
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+            #login(request, user)
+            # Redirect to a success page.
+                print 'jest dobrze'
+                return render( request, 'dashboard.html')
+        else:
+            print 'jest zle'
+            # Return a 'disabled account' error message
+
+        return render( request, 'base.html')
+
+    else:
+        return render( request, 'base.html')
+
+
+def my_view(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            # Redirect to a success page.
+        else:
+            print 'jest dobrze'
+            # Return a 'disabled account' error message
+    else:
+        print 'jest'
+        # Return an 'invalid login' error message.
+
+
+def logout_view(request):
+    logout(request)
     return render( request, 'base.html')
--- villaproject/urls.py
+++ villaproject/urls.py
@@ -1,19 +1,21 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'villaproject.main.views.home', name='home'),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'base.html'}),
+    url(r'^logout/$', 'villaproject.main.views.logout_view'),
     # url(r'^villaproject/', include('villaproject.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
